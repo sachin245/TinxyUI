cd477ad04440a3a558eb8cf35b18440147b822e4
sachin245 <sachin245@users.noreply.github.com>
2026-04-03 17:00:34 +0530

    Add deployment configs: package.json and PM2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'tinxy-ui',
+      script: 'tinxy-ui/serve.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '128M',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
